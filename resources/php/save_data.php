de772ba6dec23750866ad23cd71f34363093f852
Davi Alves Oliveira <davi-ao@users.noreply.github.com>
2019-05-02 15:43:20 -0300

    1.0.0 - Corrected DB info

--- resources/php/save_data.php
+++ resources/php/save_data.php
@@ -3,9 +3,9 @@ $type = $_POST['type'];
 $data = $_POST['data'];
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=db_ingles",
-        'root', 'acessoroot');
-    $sql = "INSERT INTO rst VALUES(0, '{$type}', '{$data}', NOW())";
+    $conn = new PDO("mysql:host=localhost;dbname=ingles_geral",
+        'ingles_user', 'Db@oegfn_IEl1', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $sql = "INSERT INTO data VALUES(0, '{$type}', '{$data}', NOW())";
     $conn->query($sql);
 
     echo $sql;
